3fdb93662c5fd63eb67d068238562f93dafe79d2
maha441997@gmail.com <maha441997@gmail.com>
2020-03-25 14:51:23 +0530

    initial

---
+++ src/components/timeline.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Member Technical Staff at ManageEngine, Zoho Corp Division <span>2018-Present</span></h2>
+                        <p>I have joined the ManageEngine's NetFlow Analyzer as a Software Developer. My major part of the work has been into the field of developing and maintaining Network monitoring software  .</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at ManageEngine, Zoho Corp Division <span>2017-2018</span></h2>
+                        <p>I joined the ManageEngine (Zoho Corp Division) as a Software Developer-Intern in the ManageEngine's NetFlow Analyzer team. Worked with peers to understand the product and develop features based on the customer requirements and developed Network traffic analysis based on ip address groups.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation at SKCT<span>2014-2018</span></h2>
+                        <p>I am pursued my under-graduation studies with major in IT.(Information Technology). I have taken courses like DSA, OOPs, Computer Networks, Advanced Java Programming, Web-Data Management over the years and have better understanding of these subjects.I have also awarded for my Academic Excellence in third year.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Higher Education <span>2012-2014</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
